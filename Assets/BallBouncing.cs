60e1ad93eec435155dd127048e937a68c447df5b
ITHUB\Student <gon4.egor@gmail.com>
2024-11-18 12:34:51 +0300

    beee

--- Assets/BallBouncing.cs
+++ Assets/BallBouncing.cs
@@ -1,19 +1,40 @@
+using System;
 using UnityEngine;
 
 public class BallBouncing : MonoBehaviour
 {
     [SerializeField] private Material[] material;
+    [SerializeField] int vel;
     private MeshRenderer mesh;
+    Rigidbody rb;
+    public bool canChangeMaterial = true;
+    float timer;
     private void Start()
     {
         mesh = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
     }
-    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (MathF.Abs(rb.velocity.y) < vel && canChangeMaterial == true)
+        {
+            timer = 0;
+            canChangeMaterial = false;
+            ChangeColor();
+        }
+        if(MathF.Abs(rb.velocity.y) > vel) canChangeMaterial = true;
+        if(canChangeMaterial == false) print(timer);
+    }
+    private void ChangeColor()
+    {
+        mesh.material = material[UnityEngine.Random.Range(0, material.Length)];
+    }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.CompareTag("Ground"))
         {
-            mesh.material = material[Random.Range(0, material.Length)];
+            ChangeColor();
         }
     }
 }
